bb33ef856616dc2d834a9a862de821b1e9730993
Shameen-ghyas <shameenghyas@gmail.com>
2026-08-04 16:22:11 +0500

    Fix Hopsworks feature group upload

--- src/hopsworks_utils.py
+++ src/hopsworks_utils.py
@@ -4,14 +4,13 @@ from config import API_KEY
 def upload_to_hopsworks(df):
     project = hopsworks.login(api_key_value=API_KEY)
     fs = project.get_feature_store()
-    fg = fs.create_feature_group(
+    fg = fs.get_feature_group(
     name="aqi_daily_features",
-    version=5,
-    primary_key=["time"],
-    description="Karachi AQI daily lag and rolling features",
-    time_travel_format="HUDI"
+    version=5
 )
+
     fg.insert(df)
+
 
 
 def load_from_hopsworks(name="aqi_daily_features", version=5):
